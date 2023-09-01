96f3aadcf90343cd143bb0b0a0572dbd1503c612
shwetansh_mi <shwetansh934@gmail.com>
2023-09-01 22:53:31 +0530

    first commit

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oaaaguufvQ1Z4IkD5d7',
+        issuer: 'https://dev-26781025.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
